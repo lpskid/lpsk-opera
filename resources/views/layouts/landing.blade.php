372d2d08fbb498014929bc236d743712e8288d6b
Arya Dwi Putra <aryadptr.developer@gmail.com>
2024-10-29 20:41:57 +0700

    kvde3

--- resources/views/layouts/landing.blade.php
+++ resources/views/layouts/landing.blade.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
 
     <title> @yield('title') - {{ config('app.name', 'Laravel') }}</title>
     {{-- favicon --}}
